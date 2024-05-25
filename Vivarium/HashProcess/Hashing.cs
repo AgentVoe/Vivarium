351552d73e28c278b0dd528c021f9a71b385ebdd
Voe <mr.subiter@mail.ru>
2024-05-25 17:11:54 +0500

    Примерный вид работы авторизации

---
+++ Vivarium/HashProcess/Hashing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vivarium.Authorization
+{
+    public class Hashing
+    {
+		private string password;
+        public Hashing(string password)
+        {
+            this.password = password;
+        }
+        private string HashPassword()
+		{
+			byte[] salt;
+			byte[] buffer2;
+			if (password == null)
+			{
+				throw new ArgumentNullException("password");
+			}
+			using (Rfc2898DeriveBytes bytes = new(password, 0x10, 0x3e8))
+			{
+				salt = bytes.Salt;
+				buffer2 = bytes.GetBytes(0x20);
+			}
+			byte[] dst = new byte[0x31];
+			Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+			Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+			return Convert.ToBase64String(dst);
+		}
+	}
+}

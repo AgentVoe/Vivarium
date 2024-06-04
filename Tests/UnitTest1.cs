fda3704a1fa6dceeeadf727a373c7104e4fb532b
Карпов Александр <karpoffSasha03@gmail.com>
2024-06-05 01:08:13 +0500

    Add new random test

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,12 +1,14 @@
 using Vivarium.Authorization;
+using Vivarium.StaticData;
+
 
 namespace Tests
 {
     [TestFixture]
-    public class Tests 
+    public class Tests
     {
         [Test]
-        public void RandomTest()
+        public void RandomPassTest()
         {
             var login = "login";
             var rndPass = new Random().Next();
@@ -18,11 +20,26 @@ namespace Tests
         }
 
         [Test]
-        public void MutatuionTest()
+        public void RandomLoginAndPassTest()
         {
+            var login = new Random().Next();
+            var rndPass = new Random().Next();
+            var authorize = new UsersAuthorization(login.ToString(), rndPass.ToString());
 
+            var res = authorize.CheckPassword();
 
-            Assert.That(true, Is.True);
+            Assert.That(res, Is.False);
+        }
+
+        [TestCase(1, 1, 1)]
+        public void MutatuionTest(int a, int b, int c)
+        {
+            var countDoneBook = UserAndBooks.userAndBooks[0].StatusBooks.Where(
+                sb => sb.Status.Status1 == "Прочитано").Count();
+
+            var res = UserAndBooks.GetCountDoneBook();
+
+            Assert.That(countDoneBook == res, Is.True);
         }
     }
 }

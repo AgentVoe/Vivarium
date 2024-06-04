8e2b8f1f503a0ffee83177965b20707c78aa2725
Карпов Александр <karpoffSasha03@gmail.com>
2024-06-04 23:55:27 +0500

    Create random test

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Vivarium.Authorization;
+
 namespace Tests
 {
     [TestFixture]
@@ -6,7 +8,21 @@ namespace Tests
         [Test]
         public void RandomTest()
         {
-            
+            var login = "login";
+            var rndPass = new Random().Next();
+            var authorize = new UsersAuthorization(login, rndPass.ToString());
+
+            var res = authorize.CheckPassword();
+
+            Assert.That(res, Is.False);
+        }
+
+        [Test]
+        public void MutatuionTest()
+        {
+
+
+            Assert.That(true, Is.True);
         }
     }
 }

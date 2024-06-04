921b2dc3e3d8dd86c2a9e13936560d22c55a4825
a7comma <iba7comma@gmail.com>
2024-06-04 19:51:50 +0500

    get data for statistics

--- Vivarium/StaticData/UserAndBooks.cs
+++ Vivarium/StaticData/UserAndBooks.cs
@@ -18,5 +18,63 @@ namespace Vivarium.StaticData
 		{
 			return userAndBooks[0].StatusBooks.Where(sb => sb.Book.Id == bookId).FirstOrDefault().Status;
         }
-	}
+
+		public static int GetCountDoneBook()
+		{
+			return userAndBooks[0].StatusBooks.Where(sb => sb.Status.Status1 == "Прочитано").Count();
+		}
+        public static int GetCountStopBook()
+        {
+            return userAndBooks[0].StatusBooks.Where(sb => sb.Status.Status1 == "Заброшено").Count();
+        }
+        public static int GetCountFutureBook()
+        {
+            return userAndBooks[0].StatusBooks.Where(sb => sb.Status.Status1 == "В планах").Count();
+        }
+        public static int GetCountInProgressBook()
+        {
+            return userAndBooks[0].StatusBooks.Where(sb => sb.Status.Status1 == "Читаю").Count();
+        }
+		public static Dictionary<string, int> GetGenresValue()
+		{
+            Dictionary<string, int> dictGenres = new Dictionary<string, int>();
+			foreach (var statusBook in userAndBooks[0].StatusBooks) 
+			{
+				foreach (var genre in statusBook.Book.BooksGenres)
+				{
+					if (dictGenres.ContainsKey(genre.Genre.GenreName))
+						dictGenres[genre.Genre.GenreName] += 1;
+					else dictGenres.Add(genre.Genre.GenreName, 1);
+                }				
+			}
+			return dictGenres;
+        }
+        public static Dictionary<string, int> GetAuthorsValue()
+        {
+            Dictionary<string, int> dictAuthors = new Dictionary<string, int>();
+            foreach (var statusBook in userAndBooks[0].StatusBooks)
+            {
+                foreach (var author in statusBook.Book.BooksAuthors)
+                {
+                    if (dictAuthors.ContainsKey(author.Author.Name))
+                        dictAuthors[author.Author.Name] += 1;
+                    else dictAuthors.Add(author.Author.Name, 1);
+                }
+            }
+            return dictAuthors;
+        }
+        public static Dictionary<string, int> GetYearValue()
+        {
+            Dictionary<string, int> dictYears = new Dictionary<string, int>();
+            foreach (var statusBook in userAndBooks[0].StatusBooks.
+                Where(sb => sb.Status.Status1 == "Прочитано"))
+            {
+                string year = statusBook.StDate.Value.Year.ToString();
+                if (dictYears.ContainsKey(year))
+                    dictYears[year] += 1;
+                else dictYears.Add(year, 1);
+            }
+            return dictYears;
+        }
+    }
 }
